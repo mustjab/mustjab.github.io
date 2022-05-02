82897b23f421445e2d2f2829e085ec280c1ad730
Mustapha Jaber <mustjab@windows.microsoft.com>
2022-05-02 12:10:52 -0700

    Add a new test pwa

---
+++ nomanifest/pwabuilder-sw.js
@@ -0,0 +1,48 @@
+const CACHE = "pwabuilder-page";
+
+
+const offlineFallbackPage = "../offline.html";
+
+self.addEventListener( "install", function( event ) {
+// console.log( "WORKER: install event in progress." );
+// immediately take over
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE)
+    .then(function( cache ) {
+      return cache.add(offlineFallbackPage);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(function (error) {
+      // The following validates that the request was for a navigation to a new document
+      if (
+        event.request.destination !== "document" ||
+        event.request.mode !== "navigate"
+      ) {
+        return;
+      }
+
+      console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
+      return caches.open(CACHE).then(function (cache) {
+        return cache.match(offlineFallbackPage);
+      });
+    })
+  );
+});
+
+self.addEventListener("refreshOffline", function () {
+  const offlinePageRequest = new Request(offlineFallbackPage);
+
+  return fetch(offlineFallbackPage).then(function (response) {
+    return caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
+      return cache.put(offlinePageRequest, response);
+    });
+  });
+});
